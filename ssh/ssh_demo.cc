f9879954d8fb6f9cc5dcb0e930d7b4dd1547d149
Ashish Thakkar <ashishvthakkar@gmail.com>
2022-01-12 20:15:51 -0500

    Use command line params for demo

--- ssh/ssh_demo.cc
+++ ssh/ssh_demo.cc
@@ -1,12 +1,22 @@
+#include <gflags/gflags.h>
+
 #include "ssh_initiator.h"
 
-int main() {
+DEFINE_string(host, "", "[localhost]");  // NOLINT
+DEFINE_string(user, "", "[username]");   // NOLINT
+DEFINE_string(                           // NOLINT
+    command,
+    "",
+    "[/home/ashish/git/scom/build/ssh/ssh_responder -request=ping]");
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
   // TODO(ashish): Remove as part of no_except change
   try {
     SshInitiator(
-        "ash-alienw-r11",
-        "ashish",
-        "/home/ashish/git/scom/build/ssh/ssh_responder -request=ping",
+        FLAGS_host,
+        FLAGS_user,
+        FLAGS_command,
         [](const char* /*buffer*/, int bytes_read, ssh::Channel& channel) {
           LOG(INFO) << "Got callback with " << bytes_read << " bytes read.";
           std::string addition_input("additional_input_from_callback\n");
--- ssh/ssh_responder.cc
+++ ssh/ssh_responder.cc
@@ -1,19 +1,12 @@
-#include <gflags/gflags.h>
-
 #include <iostream>
 
-DEFINE_string(request, "", "[ping]");  // NOLINT
-
-int main(int argc, char **argv) {
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-
-  std::cout << "Got request: " << FLAGS_request << std::endl;
+int main() {
+  std::cout << "Responder started." << std::endl;
   std::string input;
   std::cin >> input;
   std::cout << "Received input: " << input << std::endl;
-  if (FLAGS_request == "ping") {
-    std::string response("pong");
-    std::cout << "Responding with: " << response << std::endl;
-  }
+  std::string input2;
+  std::cin >> input2;
+  std::cout << "Received new input: " << input2 << std::endl;
   std::cout << "Done." << std::endl;
 }
