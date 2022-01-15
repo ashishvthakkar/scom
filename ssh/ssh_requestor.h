f71ce593c307fda2f3b2bde6630c06817f6dcae8
Ashish Thakkar <ashishvthakkar@gmail.com>
2022-01-15 18:07:00 -0500

    Update dependency specification

--- ssh/ssh_requestor.h
+++ ssh/ssh_requestor.h
@@ -12,6 +12,25 @@ public:
       const std::string& user,
       const std::string& command);
 
+  // Note: Targets
+  // 1.
+  // explicit SshRequestor(
+  //     const std::string& host,
+  //     const std::string& user,
+  //     const std::string& command);
+  // const std::vector<char>& Request(const std::vector<char>& request);
+  // stdin --- read and write (1 and 2 as file descriptor)
+
+  // 2. Perf
+  // Timestamps back and forth. Tx per second.
+  // ~1m files in <10 sec> --> 100k calls per sec
+
+  // 3. Interface
+  // template<typename T, typename U>
+  // const U& Request(const T &request);
+
+  // std::vector<std::string> Request(const std::vector<std::string> &request);
+
 private:
   bool
   SshResponseHandler(const char* buffer, int bytes_read, ssh::Channel& channel);
