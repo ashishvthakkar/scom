081b07ef697c1362dc3e781636c4c9963e33aa7e
Ashish Thakkar <ashishvthakkar@gmail.com>
2022-01-14 21:14:13 -0500

    Log responder output to log file

--- ssh/ssh_responder.cc
+++ ssh/ssh_responder.cc
@@ -1,18 +1,25 @@
+#include <config.h>
+
+#include <fstream>
 #include <iostream>
 
 #include "../protocol/protocol_adapter.h"
 
 int main() {
-  std::cout << "Responder started. <eom>" << std::endl;
+  std::cout << "Responder started and will log to " << kResponderLog << " <eom>"
+            << std::endl;
+  std::ofstream log_file;
+  log_file.open(kResponderLog, std::ios_base::app);
   std::string input;
   std::cin >> input;
-  std::cout << "Received input with size: " << input.size() << std::endl;
-  std::cout << "Received input: " << input << std::endl;
+  log_file << "Received input with size: " << input.size() << std::endl;
+  log_file << "Received input: " << input << std::endl;
   unsigned int version = 0;
   std::string details;
   scom::ReadMessage(version, details, input);
   std::cout << "Read message with version " << version
             << " and details: " << std::endl;
   std::cout << details << std::endl;
-  std::cout << "Done. <eom>" << std::endl;
+  log_file << "Sent resonse for version: " << version << std::endl;
+  log_file << "Done." << std::endl;
 }
