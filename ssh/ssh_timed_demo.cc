82cbc42a90692038361f005403794a9333508593
Ashish Thakkar <ashishvthakkar@gmail.com>
2022-01-17 17:23:55 -0500

    Take num requests for timed demo as cmd line param

--- ssh/ssh_timed_demo.cc
+++ ssh/ssh_timed_demo.cc
@@ -12,18 +12,19 @@ DEFINE_string(                           // NOLINT
     command,
     "",
     "[/home/ashish/git/scom/build/ssh/ssh_responder]");
+DEFINE_int32(num_requests, 10, "[number of requests]");  // NOLINT
 
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   std::vector<std::string> requests{"5 + 2", "3*7"};
 
-  const auto num_requests = 100'000;
+  const auto num_requests = FLAGS_num_requests;
   const auto num_repeats = num_requests / requests.size();
 
   scom::SshRequestor requestor(FLAGS_host, FLAGS_user, FLAGS_command);
   namespace chrono = std::chrono;
   auto start = chrono::high_resolution_clock::now();
-  for (int i = 0; i < num_repeats; i++) {
+  for (auto i = 0; i < num_repeats; i++) {
     for (const auto& request : requests) {
       const auto& response = requestor.SendReceive(request);
       // Disable logging during perf run
